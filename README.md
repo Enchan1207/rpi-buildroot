270501fb1677d35eafc99b7320fdd7b3502e5d13
Enchan <enchant0130@gmail.com>
2023-10-29 14:38:05 +0900

    [Update #1] unify Enchan1207/create-custom-rpi-image

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Buildroot for Raspberry Pi 3
 
 This repository builds [external toolchain](https://buildroot.org/downloads/manual/manual.html#build-toolchain-with-buildroot) and base docker image of Buildroot for Raspberry Pi 3. By using this, you can build Raspberry Pi 3 image quickly.
 
+**Update:** This repository is now available as an action in GitHub Actions.
+
 ## Contents
 
 This repository provides shown below:
@@ -13,6 +15,7 @@ This repository provides shown below:
  - **External toolchain**: Compilers, binary utilities (like assembler or linker), and a standard C library.
  - **Legal information**: License or legal informations about programs included in external toolchains.
  - **Buildroot base image**: The minimum Docker image for Buildroot to work. 
+ - **GitHub Actions action**: The custom action for inclusion and use in other repositories.
 
 External toolchain and legal information is included in [release](https://github.com/Enchan1207/rpi-buildroot/releases). Buildroot base image is published on [Docker Hub](https://hub.docker.com/repository/docker/enchan1207/buildroot_base/general).
 
---
+++ buildroot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Buildroot configuration launcher
+#
+
+# Buildrootベースイメージを探す
+baseImageName=enchan1207/buildroot
+baseImageInfo=`docker images --format json | jq -r "select(.Repository == \"${baseImageName}\")"`
+if [ -z "$baseImageInfo" ]; then
+    echo "Please build enchan1207/buildroot using setup_buildroot_image.sh before invoke $0"
+    exit 1
+fi
+
+# 成果物ディレクトリを作成
+distributionDir=dist
+mkdir -p $distributionDir
+
+# コンフィグがあるならdistにコピーする
+configFileName=buildroot.config
+if [ -e $configFileName ]; then
+    echo "Configuration file found. copy to ${distributionDir}..."
+    cp $configFileName ${distributionDir}/.config
+fi
+
+# コンテナを起動
+echo "Directory ${distributionDir} will be mounted to /${distributionDir}."
+echo "While invoke make, please use instead:"
+echo "    make O=/${distributionDir}"
+docker run --rm -it -v ./${distributionDir}:/${distributionDir} $baseImageName
+
+# 終了後、コンフィグが変更されていればdistディレクトリからコンフィグをローカルにコピーする
+if [ -e ${distributionDir}/.config ]; then
+    cmp ${distributionDir}/.config $configFileName
+    if [ $? -ne 0 ]; then
+        echo "Copy generated config from /${distributionDir} to ./${distributionDir}."
+        echo "This operation needs super-user permission (password may be required)."
+
+        # パスワードが必要になるかもしれない
+        mv ${distributionDir}/.config $configFileName
+        if [ $? -ne 0 ]; then
+            sudo mv ${distributionDir}/.config $configFileName
+        fi
+    fi
+fi
